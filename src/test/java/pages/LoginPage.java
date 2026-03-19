8a32bf0aec6618dbc394b1dd5de06c27653a9bdb
Deepika-2721 <deepikamvel27@gmail.com>
2026-03-19 21:09:44 +0530

    Create LoginPage.java

---
+++ src/test/java/pages/LoginPage.java
@@ -0,0 +1,77 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
+public class LoginPage {
+
+    WebDriver driver;
+    WebDriverWait wait;
+
+    @FindBy(name = "username")
+    WebElement usernameField;
+
+    @FindBy(name = "password")
+    WebElement passwordField;
+
+    @FindBy(xpath = "//button[@type='submit']")
+    WebElement loginButton;
+
+    @FindBy(xpath = "//p[contains(@class,'oxd-alert-content-text')]")
+    WebElement errorMessage;
+
+    @FindBy(xpath = "//h6[contains(@class,'oxd-text')]")
+    WebElement dashboardHeader;
+
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(15));
+        PageFactory.initElements(driver, this);
+    }
+
+    public void enterUsername(String username) {
+        wait.until(ExpectedConditions.visibilityOf(usernameField));
+        usernameField.clear();
+        usernameField.sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        passwordField.clear();
+        passwordField.sendKeys(password);
+    }
+
+    public void clickLogin() {
+        loginButton.click();
+    }
+
+    public DashboardPage loginAs(String username, String password) {
+        enterUsername(username);
+        enterPassword(password);
+        clickLogin();
+        return new DashboardPage(driver);
+    }
+
+    public boolean isErrorMessageDisplayed() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(errorMessage));
+            return errorMessage.isDisplayed();
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    public String getErrorMessageText() {
+        try {
+            wait.until(ExpectedConditions.visibilityOf(errorMessage));
+            return errorMessage.getText();
+        } catch (Exception e) {
+            return "";
+        }
+    }
+}
