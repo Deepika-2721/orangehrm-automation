cd2ad4d5600e6dc2048e9eb368f69ab5aeefc354
Deepika-2721 <deepikamvel27@gmail.com>
2026-03-19 21:11:43 +0530

    Create BaseTest.java

---
+++ src/test/java/utils/BaseTest.java
@@ -0,0 +1,37 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.time.Duration;
+
+public class BaseTest {
+
+    protected WebDriver driver;
+
+    public static final String BASE_URL = "https://opensource-demo.orangehrmlive.com/web/index.php/auth/login";
+    public static final String ADMIN_USER = "Admin";
+    public static final String ADMIN_PASS = "admin123";
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--start-maximized");
+        options.addArguments("--disable-notifications");
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        driver.get(BASE_URL);
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+}
